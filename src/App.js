b53f69896c5764a2bbc1af259ef5f85d96f67883
erensanlier <erensanlier@gmail.co>
2021-03-30 14:47:26 +0300

    Amplify configuration implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react'
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
